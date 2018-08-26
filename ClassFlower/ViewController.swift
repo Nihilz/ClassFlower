b9057a5da758f2b169772602d197700244da14d1
Andrew Pepper <andrewpepper@andrews-mini>
2018-08-26 15:20:10 +0100

    Prepare userImage for request. Create request for MLModel. Handle request. Update UI with result.

--- ClassFlower/ViewController.swift
+++ ClassFlower/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -25,12 +26,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        let userPickedImage = info[UIImagePickerControllerOriginalImage]
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
         
-        displayPhotoImage.image = userPickedImage as? UIImage
+            guard let convertedCIImage = CIImage(image: userPickedImage) else {
+                fatalError("Cannot convert to CIImage")
+            }
+            
+            detect(image: convertedCIImage)
+        
+            displayPhotoImage.image = userPickedImage
+            
+        }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Error importing model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+        try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
